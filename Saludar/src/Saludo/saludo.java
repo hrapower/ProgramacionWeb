556131296a563b10ebfe799a53ef2ff425713533
hrapower <94088527+hrapower@users.noreply.github.com>
2022-09-09 10:41:43 -0500

    Add files via upload

---
+++ Saludar/src/Saludo/saludo.java
@@ -0,0 +1,14 @@
+package Saludo;
+/*
+ * xd
+ */
+
+public class saludo {
+
+	public static void main(String[] args) {
+		System.out.println("saludos desde barrancos hijos de la wallava");
+	
+
+	}
+
+}
